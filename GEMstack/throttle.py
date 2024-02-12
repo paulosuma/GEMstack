b1297c499e4fa38159286c8c8224dc838f937c26
abhisathya4 <73118914+abhisathya4@users.noreply.github.com>
2024-02-12 11:43:01 -0600

    Update throttle.py

--- GEMstack/throttle.py
+++ GEMstack/throttle.py
@@ -9,63 +9,119 @@ from pacmod_msgs.msg import (
     VehicleSpeedRpt,
 )
 
+
 # For message format, see
 # https://github.com/astuff/astuff_sensor_msgs/blob/3.3.0/pacmod_msgs/msg/PacmodCmd.msg
-
 class ThrottleInput:
     """Your control loop code should go here.  You will use ROS and Pacmod messages
     to communicate with drive-by-wire system to control the vehicle's turn signals.
     """
 
     def __init__(self, throttle_level):
-        # TODO: Initialize your publishers and subscribers here
+        # Throttle Pedal Publisher
         self.accel_pub = rospy.Publisher(
-            "/pacmod/as_rx/accel_cmd", PacmodCmd, queue_size=12
+            "/pacmod/as_rx/accel_cmd", PacmodCmd, queue_size=1
         )
+        # Brake Pedal Publisher
         self.brake_pub = rospy.Publisher(
-            "/pacmod/as_rx/brake_cmd", PacmodCmd, queue_size=12
+            "/pacmod/as_rx/brake_cmd", PacmodCmd, queue_size=1
         )
-        self.throttle_level = throttle_level
-        # When you create a subscriber to a /pacmod/parsed_tx/X topic, you will need to provide a callback function.
-        # You will want this callback to be a BlinkDistress method, such as print_X(self, msg).  msg will have a
-        # ROS message type, and you can find out what this is by either reading the documentation or running
-        # "rostopic info /pacmod/parsed_tx/X" on the command line.
+        # Steering Column Publisher
+        self.steer_pub = rospy.Publisher(
+            "/pacmod/as_rx/steer_cmd", PositionWithSpeed, queue_size=1
+        )
+        # Speed Subscriber
+        self.speed_sub = rospy.Subcriber(
+            "/pacmod/parsed_tx/vehicle_speed_rpt",
+            VehicleSpeedRpt,
+            self.print_speed_to_file,
+        )
+        # Steering Column Subscriber
+        self.steer_sub = rospy.Subcriber(
+            "/pacmod/parsed_tx/steer_rpt", SystemRptFloat, self.get_steering_position
+        )
 
-        pass
+        # Member Variables
+        self.throttle_level = throttle_level
+        self.file = open(f"/logs/accel_log_{throttle_level}.csv", "w")
+        self.vehicle_speed = 0
+        self.steering_position = -1
+
+    def print_speed_to_file(self, data):
+        """Print Vehicle Speed"""
+        timestamp = data.header.stamp
+        speed = data.vehicle_speed
+        valid = data.valid
+        if valid:
+            self.vehicle_speed = speed
+            self.file.write(f"{timestamp.secs*(10**9) + timestamp.nsecs},{speed}\n")
+
+    def get_steering_position(self, data):
+        self.steering_position = max(
+            abs(data.manual_input), abs(data.command), abs(data.output)
+        )
 
     def rate(self):
         """Requested update frequency, in Hz"""
-        return 2
+        return 3
 
     def initialize(self):
         """Run first"""
         pass
 
     def cleanup(self):
-        """Run last"""
-        accel_cmd = PacmodCmd()
-        accel_cmd.enable = True
-        accel_cmd.f64_cmd = 0
-        self.accel_pub.publish(accel_cmd)
+        """Run last
+        Sets throttle pedal position to 0 and brake pedal position to 0.7 for
+        5 seconds."""
+        rate = rospy.Rate(rate)
+        max_calls = 15
+        num_calls = 0
+        while self.vehicle_speed > 0 and num_calls < max_calls:
+            # Set Throttle Pedal Position to 0
+            accel_cmd = PacmodCmd()
+            accel_cmd.enable = True
+            accel_cmd.f64_cmd = 0
+            self.accel_pub.publish(accel_cmd)
+            # Set Brake Pedal Position to 0.7
+            brake_cmd = PacmodCmd()
+            brake_cmd.enable = True
+            brake_cmd.f64_cmd = 0.7
+            # Set Steering Column to Straight
+            steer_cmd = PositionWithSpeed()
+            steer_cmd.angular_position = 0.0
+            steer_cmd.angular_velocity_limit = 1
+            self.steer_pub.publish(steer_cmd)
+            num_calls += 1
+            rate.sleep()
 
-        brake_cmd = PacmodCmd()
-        brake_cmd.enable = True
-        brake_cmd.f64_cmd = 0.7
+        self.file.close()
 
     def update(self):
         """Run in a loop"""
-        # TODO: Implement your control loop here
         rate = rospy.Rate(rate)
-
         while not rospy.is_shutdown():
+            # Set steering column to center before moving
+            while not self.steering_position == 0:
+                steer_cmd = PositionWithSpeed()
+                steer_cmd.angular_position = 0.0
+                steer_cmd.angular_velocity_limit = 1
+                self.steer_pub.publish(steer_cmd)
+                rate.sleep()
+
+            # Once steering column is centered - move
             accel_cmd = PacmodCmd()
             accel_cmd.enable = True
-            accel_cmd.f64_cmd = self.throttle_level
-            self.accel_pub.publish(accel_cmd)
+            accel_cmd.f64_cmd = self.throttle_level 
+            self.accel_pub.publish(accel_cmd)  # Set Throttle
+            brake_cmd = PacmodCmd()
+            brake_cmd.enable = True
+            brake_cmd.f64_cmd = 0
+            self.brake_pub.publish(brake_cmd)  # Set Brake
+            steer_cmd = PositionWithSpeed()
+            steer_cmd.angular_position = 0.0
+            steer_cmd.angular_velocity_limit = 1
+            self.steer_pub.publish(steer_cmd)  # Set Steering
             rate.sleep()
-        # You will need to publish a PacmodCmd() to /pacmod/as_rx/turn_cmd.  Read the documentation to see
-        # what the data in the message indicates.
-        pass
 
     def healthy(self):
         """Returns True if the element is in a stable state."""
