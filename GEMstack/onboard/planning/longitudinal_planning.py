44d247770e84921ffd6835486d97ebfd18f3acde
Ardor-Wu <70617529+Ardor-Wu@users.noreply.github.com>
2024-02-18 20:05:13 -0600

    qilong HW2 Q3

--- GEMstack/onboard/planning/longitudinal_planning.py
+++ GEMstack/onboard/planning/longitudinal_planning.py
@@ -4,6 +4,7 @@ from ...state import AllState, VehicleState, EntityRelation, EntityRelationEnum,
     ObjectFrameEnum
 from ...utils import serialization
 from ...mathutils.transforms import vector_madd
+import numpy as np
 
 
 def longitudinal_plan(path: Path, acceleration: float, deceleration: float, max_speed: float,
@@ -16,6 +17,7 @@ def longitudinal_plan(path: Path, acceleration: float, deceleration: float, max_
     3. if at any point you can't brake before hitting the end of the path,
        decelerate with accel = -deceleration until velocity goes to 0.
     """
+    '''
     path_normalized = path.arc_length_parameterize()
     points = [p for p in path_normalized.points]
 
@@ -29,14 +31,112 @@ def longitudinal_plan(path: Path, acceleration: float, deceleration: float, max_
         times.append(xt)
         xt += frag
     print(points, times)
+    '''
 
-    trajectory = Trajectory(path.frame, points, times)
+    path_normalized = path.arc_length_parameterize()
+    points = [p for p in path_normalized.points]
+    theta = np.arctan2(points[-1][1] - points[0][1], points[-1][0] - points[0][0])
+    acceleration, deceleration, max_speed, current_speed = acceleration * np.cos(theta), deceleration * np.cos(
+        theta), max_speed * np.cos(theta), current_speed * np.cos(theta)
 
+    sp = points[0][0]
+    ep = points[-1][0]
+    length = ep - sp
+    s_stop = current_speed ** 2 / (2 * deceleration)
+    if s_stop > length:
+        points, times = decelerate(sp, deceleration, current_speed)
+        trajectory = Trajectory(path.frame, points, times)
+        return trajectory
+
+    if acceleration == 0:
+        # t_const=current_speed/deceleration
+        s_const = length - s_stop
+        t_const = s_const / current_speed
+        points_const, times_const = const_speed(sp, sp + s_const, current_speed, 0)
+        points_dec, times_dec = decelerate(sp + s_const, deceleration, current_speed, t_const)
+        trajectory = Trajectory(path.frame, points_const + points_dec, times_const + times_dec)
+        return trajectory
+
+    s_th = (max_speed ** 2 - current_speed ** 2) / (2 * acceleration) + max_speed ** 2 / (2 * deceleration)
+
+    # s_acc: float = max_speed ** 2 / (2 * acceleration)
+    # s_con = length - s_dec - s_acc
+
+    if s_th > length:
+        v_max = np.sqrt(
+            (length + current_speed ** 2 / (2 * acceleration))
+            /
+            (1 / (2 * acceleration) + 1 / (2 * deceleration))
+        )
+        time_acc = (v_max - current_speed) / acceleration
+        points_acc, times_acc = accelerate(sp, acceleration, current_speed, v_max)
+        s_dec = v_max ** 2 / (2 * deceleration)
+        points_dec, times_dec = decelerate(ep - s_dec, deceleration, v_max, time_acc)
+
+        trajectory = Trajectory(path.frame, points_acc + points_dec, times_acc + times_dec)
+        return trajectory
+
+    s_acc = (max_speed ** 2 - current_speed ** 2) / (2 * acceleration)
+    s_dec = max_speed ** 2 / (2 * deceleration)
+    t_acc = (max_speed - current_speed) / acceleration
+    t_const = (length - s_acc - s_dec) / max_speed
+    t_dec = max_speed / deceleration
+    points_acc, times_acc = accelerate(sp, acceleration, current_speed, max_speed)
+    points_const, times_const = const_speed(sp + s_acc, ep - s_dec, max_speed, t_acc)
+    points_dec, times_dec = decelerate(ep - s_dec, deceleration, max_speed, t_acc + t_const)
+    trajectory = Trajectory(path.frame, points_acc + points_const + points_dec, times_acc + times_const + times_dec)
     return trajectory
+    # raise NotImplementedError("Not implemented yet")
+
+    # points_dacc, times_dacc = decelerate(max(ep - s_dacc, sp), deceleration, current_speed)
+    # points_acc, times_acc = [], []
+    # points_const, times_const = [], []
+    # if ep > s_dacc:
+    #    s_acc = max_speed ** 2 / (2 * acceleration)
+    #    if s_acc > length - s_dacc:
+    #        points, times = accelerate(ep - s_dacc, acceleration, current_speed, max_speed)
+
+    # need to accelerate and deaccelerate
+    # s_acc = max_speed ** 2 / (2 * acceleration)
+    # if s_acc > length - s_dacc:
+    # points, times = accelerate(ep - s_dacc, accceleration, current_speed, max_speed)
+    # only need to accelerate
 
 
-def longitudinal_brake(path: Path, deceleration: float, current_speed: float) -> Trajectory:
+def const_speed(sp, ep, speed, t0=0.0) -> (List, List):
+    n_points = 100
+    time_stop = (ep - sp) / speed
+    times = list(np.linspace(t0, time_stop + t0, n_points))
+    points = [(sp + speed * (t - t0), 0) for t in times]
+    return points, times
+
+
+def accelerate(sp, acceleration: float, current_speed: float, max_speed, t0=0) -> (List, List):
+    n_points = 100
+    time_acc = (max_speed - current_speed) / acceleration
+    times = list(np.linspace(t0, t0 + time_acc, n_points))  # [i * time_stop / n_points for i in range(n_points)]
+    points = [(sp + current_speed * (t - t0) + 0.5 * acceleration * (t - t0) ** 2, 0) for t in times]
+
+    return points, times
+
+
+def decelerate(sp, deceleration: float, current_speed: float, t0=0.0) -> (List, List):
+    if current_speed == 0:
+        return [(sp, t0), (sp, t0)], [0, 0.1]
+
+    n_points = 100
+    time_stop = current_speed / deceleration
+    # times = [i * time_stop / n_points + t0 for i in range(n_points)]
+    # divide the time into n_points, end point included using numpy
+    times = list(np.linspace(t0, time_stop + t0, n_points))
+    points = [(sp + current_speed * (t - t0) - 0.5 * deceleration * (t - t0) ** 2, 0) for t in times]
+
+    return points, times
+
+
+def longitudinal_brake(path: Path, deceleration: float, current_speed: float, t0=0) -> Trajectory:
     """Generates a longitudinal trajectory for braking along a path."""
+    '''
     path_normalized = path.arc_length_parameterize()
     points = [p for p in path_normalized.points]
     print(points)
@@ -58,6 +158,11 @@ def longitudinal_brake(path: Path, deceleration: float, current_speed: float) ->
         xt += frag
     times.append(path_normalized.times[-1])
     print(points, times)
+    '''
+    path_normalized = path.arc_length_parameterize()
+    points = [p for p in path_normalized.points]
+    sp = points[0][0]
+    points, times = decelerate(sp, deceleration, current_speed)
     trajectory = Trajectory(path.frame, points, times)
     return trajectory
 
