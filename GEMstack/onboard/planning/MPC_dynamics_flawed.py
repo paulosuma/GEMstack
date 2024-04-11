8310a1ba5348503c49354584f32557e341d4e9ba
obica-ai <dave8898@qq.com>
2024-04-10 20:52:36 -0500

    some idea of dynamic model

---
+++ GEMstack/onboard/planning/MPC_dynamics_flawed.py
@@ -0,0 +1,335 @@
+import time
+import yaml
+import numpy as np
+import casadi as ca
+from ...state.vehicle import VehicleState, ObjectFrameEnum
+from ...state.trajectory import Path, Trajectory, compute_headings
+from ..component import Component
+from ...knowledge.vehicle.geometry import front2steer
+from ...knowledge.vehicle.geometry import steer2front
+from scipy.interpolate import interp1d
+from ...utils import settings
+from ...mathutils import transforms        
+
+class MPC(object):
+    def __init__(self, dt, horizon, Q, R):
+        # Defining the tunable parameters
+        self.dt = dt
+        self.horizon = horizon # horizon represented in seconds
+        self.timesteps = int(self.horizon / self.dt) # horizon represented in the number of time steps
+        self.Q = Q
+        self.R = R
+        self.look_ahead = 2.0
+        self.look_ahead_scale = 1.0
+        self.prev_yaw_angle = None
+        self.prev_ref_yaw = None
+        self.path_arg = None
+        self.path = None 
+        self.trajectory = None
+
+        # Defining geometry and dynamic constraints on the vehicle
+        # self.front_wheel_angle_scale = 3.0
+        self.yaw_moment_of_inertia = settings.get('vehicle.dynamics.yaw_moment_of_inertia')
+        self.vehicle_mass = settings.get('vehicle.dynamics.mass')
+        self.distance_front_to_cg = settings.get('vehicle.dynamics.lf')
+        self.distance_rear_to_cg = settings.get('vehicle.dynamics.lr')
+        self.aerodynamic_drag_coefficient = settings.get('vehicle.dynamics.aerodynamic_drag_coefficient')
+        self.g = settings.get('vehicle.dynamics.gravity')
+        self.front_cornering_stiffness = settings.get('vehicle.dynamics.front_cornering_stiffness')
+        self.rear_cornering_stiffness = settings.get('vehicle.dynamics.rear_cornering_stiffness')
+
+        self.wheelbase = settings.get('vehicle.geometry.wheelbase')
+        self.max_deceleration = settings.get('vehicle.limits.max_deceleration')
+        self.max_acceleration = settings.get('vehicle.limits.max_acceleration')
+        self.min_wheel_angle = settings.get('vehicle.geometry.min_wheel_angle')
+        self.max_wheel_angle = settings.get('vehicle.geometry.max_wheel_angle')
+        self.min_steering_angle = settings.get('vehicle.geometry.min_steering_angle')
+        self.max_steering_angle = settings.get('vehicle.geometry.max_steering_angle')
+
+        self.t_last = None
+
+
+    def set_path(self, path: Path):
+        if path == self.path_arg:
+            return
+        self.path_arg = path
+        if len(path.points[0]) > 2:
+            path = path.get_dims([0, 1])
+        if not isinstance(path, Trajectory):
+            self.path = path.arc_length_parameterize()
+            self.trajectory = None
+            self.current_traj_parameter = 0.0
+        else:
+            self.path = path.arc_length_parameterize()
+            self.trajectory = path
+            self.current_traj_parameter = self.trajectory.domain()[0]
+        self.current_path_parameter = 0.0
+
+    def get_model_equations(self, states, controls):
+
+        # dynamics
+        x = states[0]  # the x position, in the object's frame. 
+        y = states[1]  # the y position, in the object's frame. 
+        theta = states[2]  #  the optional yaw, in radians and in the object's frame. If
+            # frame=GLOBAL, this is heading CW from north.  Otherwise, it is
+            # CCW yaw.
+
+        v_x = states[3] # longitudinal vel
+        v_y = states[4] # lateral vel
+        omega = states[5] # yaw rate of change
+
+        epsilon = 1e-8
+        throttle = controls[0]
+        delta = controls[1]
+        
+        m = self.vehicle_mass
+        Iz = self.yaw_moment_of_inertia
+        lf = self.distance_front_to_cg
+        lr = self.distance_rear_to_cg
+        Cf = self.front_cornering_stiffness
+        Cr = self.rear_cornering_stiffness
+
+        # x, y, theta, v_x, v_y, omega = states  # Adding lateral velocity (v_y) and yaw rate (omega) to the states
+        # throttle, delta = controls  # Throttle (or brake) and steering angle delta as controls
+
+        # Aerodynamic and gravitational forces
+        F_aero = self.aerodynamic_drag_coefficient * v_x**2
+        F_gravity = m * self.g * ca.cos(theta)
+        Fz_front = m * self.g * (lr / (lf + lr))
+        Fz_rear = m * self.g * (lf / (lf + lr))
+
+        # Tire forces
+        alpha_f = delta - ca.arctan((omega * lf + v_y) / (v_x + epsilon))
+        alpha_r = -ca.arctan((omega * lr - v_y) / (v_x + epsilon))
+        Fyf = Cf * alpha_f
+        Fyr = Cr * alpha_r
+
+        #motion
+        x_next = x + (v_x * ca.cos(theta) - v_y * ca.sin(theta)) * self.dt  # Next x position
+        y_next = y + (v_x * ca.sin(theta) + v_y * ca.cos(theta)) * self.dt  # Next y position
+        theta_next = theta + omega * self.dt  # Next orientation
+
+        # Update velocities
+        dv_x = (throttle - F_aero - F_gravity + Fyf * ca.sin(delta)) / m + v_y * omega  # Change in longitudinal velocity
+        dv_y = (Fyf * ca.cos(delta) + Fyr) / m - v_x * omega  # Change in lateral velocity
+        v_x_next = v_x + dv_x * self.dt  # Next longitudinal velocity
+        v_y_next = v_y + dv_y * self.dt  # Next lateral velocity
+
+        # Update yaw rate
+        domega = (lf * Fyf * ca.cos(delta) - lr * Fyr) / Iz  # Change in yaw rate
+        omega_next = omega + domega * self.dt  # Next yaw rate
+
+    # Return the predicted next state
+        return ca.vertcat(x_next, y_next, theta_next, v_x_next, v_y_next, omega_next)
+        # kinematics
+        # x_next = states[0] + states[3] * ca.cos(states[2]) * self.dt
+        # y_next = states[1] + states[3] * ca.sin(states[2]) * self.dt
+        # theta_next = states[2] + (states[3] / self.wheelbase) * ca.tan(controls[1]) * self.dt
+        # v_next = states[3] + controls[0] * self.dt
+        
+     
+
+        return ca.vertcat(dx, dy, dtheta, dv_x, dv_y, domega)
+        # return ca.vertcat(x_next,y_next,theta_next,v_next)
+
+    def get_cost_function(self, states, controls, ref_trajectory,opti):
+        # Implement the cost function using CasADi
+        cost = 0
+        t = ref_trajectory.times[0]
+        i = 0
+        print("reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",ref_trajectory)
+        while t < ref_trajectory.times[0] + self.horizon:
+
+            v_x = ref_trajectory.eval_derivative(t)[0]
+            v_y = ref_trajectory.eval_derivative(t)[1]
+            ref_v = np.sqrt(v_x ** 2 + v_y ** 2)
+            # print("Time step:", t)
+            # print("States:", opti.debug.value(states[:, t]))
+            # print("Controls:", opti.debug.value(controls[:, t]))
+
+            # penalizes x and y deviations
+            cost += self.Q[0] * (ref_trajectory.eval(t)[0] - states[0, i])**2 + \
+                self.Q[1] * (ref_trajectory.eval(t)[1] - states[1, i])**2
+            
+            # penalizes heading angle and velocity deviations
+            # cost += self.Q[2] * (ref_trajectory.eval(t)[2] - states[2, i])**2 + \
+            #     self.Q[3] * (ref_v - states[3, i])**2
+            
+            # penalizes heading angle and velocity deviations (forward vel and lateral vel)
+            cost += self.Q[2] * (ref_trajectory.eval(t)[2] - states[2, i])**2 + \
+                self.Q[3] * (v_x - states[3, i])**2 + self.Q[4] * (v_y - states[4,i])**2 
+            
+            #TODO
+            # if t ==ref_trajectory.times[0] :
+            #     cost += self.Q[4] * ( (ref_trajectory.eval(t)[2] /self.dt) - states[5,i] ) **2
+            # else:
+            #     cost += self.Q[4] * ( ( ( ref_trajectory.eval(t)[2] - self.prev_ref_yaw ) /self.dt) - states[5,i])**2
+            
+            # penalizes large control inputs
+            cost += self.R[0] * controls[0, i] ** 2 + self.R[1] * controls[1, i] ** 2
+
+            # penalizes large fluctuations in consecutive controls
+            if i >= 1: 
+                cost += self.R[2] * (controls[0, i] - controls[0, i - 1]) ** 2 + \
+                    self.R[3] * (controls[1, i] - controls[1, i - 1]) ** 2
+            t += self.dt
+            i += 1
+            
+            self.prev_ref_yaw = ref_trajectory.eval(t)[2]
+        return cost
+
+    def compute(self, state: VehicleState, component: Component = None):
+        assert state.pose.frame != ObjectFrameEnum.CURRENT
+        t = state.pose.t
+
+        if self.t_last is None:
+            self.t_last = t
+        dt = t - self.t_last
+        # given foward velocity, calculate lateral velocity = forward velocity * tan(steer angle)
+        #TODO given yaw, x, y position,and forward vel, calculate , yaw rate of change/ lateral vel
+        lateral_v = state.v * ca.tan(state.pose.yaw)
+        # yaw rate of change
+        # if self.prev_yaw_angle is None:
+            # domega = state.pose.yaw  / self.dt
+        # else:
+        #     domega = (state.pose.yaw - self.prev_yaw_angle) / self.dt
+
+        domega = (state.v * ca.tan(state.front_wheel_angle))/ self.wheelbase
+
+        
+        #current_state = [state.pose.x, state.pose.y, state.pose.yaw if state.pose.yaw is not None else 0.0, state.v]
+        current_state = [state.pose.x, state.pose.y, state.pose.yaw if state.pose.yaw is not None else 0.0, state.v,lateral_v,domega]
+        
+        if self.path is None:
+            raise RuntimeError("Behavior without path not implemented")
+
+        if self.path.frame != state.pose.frame:
+            print("Transforming path from", self.path.frame.name, "to", state.pose.frame.name)
+            self.path = self.path.to_frame(state.pose.frame, current_pose=state.pose)
+        if self.trajectory is not None:
+            if self.trajectory.frame != state.pose.frame:
+                print("Transforming trajectory from", self.trajectory.frame.name, "to", state.pose.frame.name)
+                self.trajectory = self.trajectory.to_frame(state.pose.frame, current_pose=state.pose)
+
+        closest_dist,closest_parameter = self.path.closest_point_local((state.pose.x,state.pose.y),[self.current_path_parameter-5.0,self.current_path_parameter+5.0])
+        self.current_path_parameter = closest_parameter
+        self.current_traj_parameter += dt
+
+        des_parameter = closest_parameter + self.look_ahead + self.look_ahead_scale * state.v
+        print("Desired parameter: " + str(des_parameter),"distance to path",closest_dist)
+        print(self.path.parameter_to_time(des_parameter))
+
+        # Slice a range of trajectory given the horizon value
+        ref_trajectory = self.path.trim(des_parameter, min(des_parameter + self.timesteps, self.path.times[-1]))
+        ref_trajectory = compute_headings(ref_trajectory)
+        # print("CURRENT STATE: ", current_state)
+        # print("REF TRAJECTORY: ", ref_trajectory)
+        # print("LEN OF REF: ", len(ref_trajectory.times))
+
+        # Set up the optimization problem
+        opti = ca.Opti()
+        # kinematics model variables
+        # x_vars = opti.variable(4, self.timesteps + 1)  # State variables
+        # u_vars = opti.variable(2, self.timesteps) # Control variables
+        # dynamics model variables
+        x_vars = opti.variable(6, self.timesteps + 1)  # State variables
+        u_vars = opti.variable(2, self.timesteps) # Control variables
+       
+        # Set initial conditions
+        opti.subject_to(x_vars[:, 0] == current_state)
+
+        for t in range(self.timesteps):
+            # Model equations constraints
+            #TODO objective function?
+            x_next = self.get_model_equations(x_vars[:, t], u_vars[:, t])
+            opti.subject_to(x_vars[:, t + 1] == x_next)
+
+            # State constraints
+            # opti.subject_to(x_vars[:, t] < self.max_wheel_angle)
+            # opti.subject_to(x_vars[:, t] > self.min_wheel_angle)
+            
+            # Control input constraints
+            opti.subject_to(opti.bounded(-self.max_deceleration, u_vars[0, t], self.max_acceleration))
+            opti.subject_to(opti.bounded(self.min_wheel_angle, u_vars[1, t], self.max_wheel_angle))
+            
+        # Set the objective function
+        #TODO two more var need to be consider in cost
+        obj = self.get_cost_function(x_vars, u_vars, ref_trajectory,opti)
+        
+        opti.minimize(obj)
+        
+        # options = {'ipopt': {
+        #     'tol': 1e-8,
+        #     'max_iter': 5000,
+        #     # 'linear_solver': 'MA57',  # Assuming you have it available
+        #     # 'derivative_test': 'none',  # Turn off for performance
+        #     # 'nlp_scaling_method': 'none',
+        #     # 'constr_viol_tol': 1e-6
+        #     # 'warm_start_init_point': 'no',  # Use 'yes' for warm starts
+        #     # 'print_level': 1
+        # }}
+        options = {
+        'ipopt': {
+        'max_iter': 20000,
+        'tol': 1e-6,
+        'dual_inf_tol': 1e-6,
+        'constr_viol_tol': 1e-6,
+        'acceptable_tol': 1e-5,  # Looser tolerance for accepting convergence
+        'linear_solver': 'mumps',  # Robust linear solver
+        'print_level': 5,  # Provides detailed output about solver progress and issues
+        'hessian_approximation': 'limited-memory',  # Good for large-scale problems
+        'derivative_test': 'first-order',  # Check derivatives (gradient) correctness63254
+    }
+}
+
+        # Set up the solver
+        opti.solver('ipopt', options)
+        
+        # Solve the optimization problem
+        sol = opti.solve()
+
+        
+        
+        # Extract the optimal control inputs
+        optimal_control = sol.value(u_vars)
+
+        # Extract the optimal steering angle and acceleration
+        optimal_acceleration = optimal_control[0, 0]
+        optimal_steering = optimal_control[1, 0]
+
+        # Convert the steering angle to the corresponding steering wheel angle
+        steering_wheel_angle = optimal_steering
+
+        return optimal_acceleration, steering_wheel_angle
+
+class MPCController(Component):
+    def __init__(self, vehicle_interface=None, **args):
+        self.MPC = MPC(**args)
+        self.vehicle_interface = vehicle_interface
+
+    def rate(self):
+        return 5.0
+
+    def state_inputs(self):
+        return ['vehicle', 'trajectory']
+
+    def state_outputs(self):
+        return []
+
+    def update(self, vehicle: VehicleState, trajectory: Trajectory):
+        start_time = time.perf_counter()
+        self.MPC.set_path(trajectory)
+        acceleration, wheel_angle = self.MPC.compute(vehicle)
+        print("curr_vel",vehicle.v)
+        print("acceleration: ", acceleration)
+        print("wheel_angle", wheel_angle)
+        steering_wheel_angle = np.clip(front2steer(wheel_angle), self.MPC.min_steering_angle, self.MPC.max_steering_angle)
+        self.vehicle_interface.send_command(self.vehicle_interface.simple_command(acceleration, steering_wheel_angle, vehicle))
+
+        end_time = time.perf_counter()
+        execution_time = end_time - start_time
+        print(f"Execution time: {execution_time:.4f} seconds")
+
+    def healthy(self):
+        return self.MPC.path is not None
+    
