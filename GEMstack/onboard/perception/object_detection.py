0897437097815eb4198ba174e74e3483cf83919c
Vishakh Suresh Babu <vishakhsuresh98@gmail.com>
2024-04-21 23:45:51 -0500

    Updated object detector to use pixel to 3d coord handler

--- GEMstack/onboard/perception/object_detection.py
+++ GEMstack/onboard/perception/object_detection.py
@@ -4,6 +4,7 @@ from ...mathutils import collisions
 from ..interface.gem import GEMInterface
 from ..component import Component
 from .point_cloud_manipulation import transform_point_cloud
+from .pixelwise_3D_lidar_coord_handler import PixelWise3DLidarCoordHandler
 
 from ultralytics import YOLO
 import numpy as np
@@ -21,21 +22,22 @@ class ObjectDetector():
         self.detector = detector
 
     def box_to_object(self, bbox_xywh):
-        """Creates a PhysicalObject from a (x,y,w,h) bounding box.
-        
+        """Creates a PhysicalObject from a (x,y,w,h) bounding box."""
+
+        x, y, w, h = bbox_xywh
+        # print('Bbox: [{.2f}, {.2f}, {.2f}, {.2f}]'.format(x,y,w,h))
+
+        """
         Uses the image, the camera intrinsics, the lidar point cloud, 
         and the calibrated camera / lidar poses to get a good estimate 
         of the object's pose (in vehicle frame) and dimensions.
         """
-
+        '''
         # Obtain point clouds in image frame and vehicle frame
         pcd_image_pixels, pcd_vehicle_frame = transform_point_cloud(
             self.lidar_point_cloud, np.array(self.camera_info.P).reshape(3,4), 
             self.camera_info.width, self.camera_info.height
         )
-
-        x, y, w, h = bbox_xywh
-        # print('Bbox: [{.2f}, {.2f}, {.2f}, {.2f}]'.format(x,y,w,h))
 
         # tolerance for calibration-related errors
         tolerance = settings.get('perception.object_detection.tolerance_factor') 
@@ -43,11 +45,22 @@ class ObjectDetector():
                     (x - tolerance * w/2) <= pcd_image_pixels[i][0] <= (x + tolerance * w/2) and 
                     (y - tolerance * h/2) <= pcd_image_pixels[i][1] <= (y + tolerance * h/2)]
         points = [pcd_vehicle_frame[idx] for idx in indices]   # in vehicle frame
+        '''
+
+        # Obtain 3d world coordinates for all pixels in the image
+        handler = PixelWise3DLidarCoordHandler()
+        all_points = handler.get3DCoord(self.camera_image, self.lidar_point_cloud) # img ht x img width x 3
+
+        # Filter points in bbox
+        points = []
+        for i in range(x - w/2, x + w/2 + 1):
+            for j in range(y - h/2, y + h/2 + 1):
+                points.append(all_points[j][i])
 
         # Estimate center and dimensions
         center = np.mean(points, axis=0)
         dimensions = np.max(points, axis=0) - np.min(points, axis=0)
-
+        
         # For a PhysicalObject, 
         #   origin is at the object's center in the x-y plane and at the bottom in the z axis
         pose = ObjectPose(t=0, x=center[0], y=center[1], z=center[2] - dimensions[2]/2, 
