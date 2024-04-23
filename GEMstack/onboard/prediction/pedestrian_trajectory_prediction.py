944254fc23f253a760209b780895c5dc429221b1
Tyler <tyler@DESKTOP-M3C7V88>
2024-04-22 22:16:17 -0500

    Revising the pedestrian_trajectory_prediction code to account for how we are now outputting a Tensor when running the model instead of a bunch of files. Code is still not complete though, but made the adjustments for the difference in output.

--- GEMstack/onboard/prediction/pedestrian_trajectory_prediction.py
+++ GEMstack/onboard/prediction/pedestrian_trajectory_prediction.py
@@ -13,6 +13,10 @@ import sys
 import time
 from collections import defaultdict
 
+# 8 frames used to base future trajectories off of (current frame plus previous 7)
+NUM_PREV_FRAMES = 7
+NUM_FUTURE_FRAMES = 12
+
 def start_model_process(model_path, env_path):
     python_path = '/root/miniconda3/envs/AgentFormer/bin/python'
     return subprocess.Popen(
@@ -60,34 +64,67 @@ class PedestrianTrajPrediction(Component):
     def estimate_velocity(self, past_values):
         # estimate velocity from past few frames
         pass
-
-    def convert_data_from_model_output(self, file_name) -> Dict[List[AgentState]]:
-        # read the file and convert the data to AgentState objects
-        agent_dict = defaultdict(list)
-        file = open(file_name, 'r')
+    
+    # def convert_data_from_model_output(self, file_name) -> Dict[List[AgentState]]:
+    # Changing signature of model since we changed the output format of AgentFormer to return the actual tensor
+    def convert_data_from_model_output(self, sample_model_3D, frame) -> List[Dict[List[AgentState]]]:
+        # # read the file and convert the data to AgentState objects
+        # agent_dict = defaultdict(list) # Key: Pedestrian ID | Value: List of AgentStates for each future frame
+        # Commenting out the code that reads lines from a file because we changed the output format of AgentFormer to return the actual tensor
+        # file = open(file_name, 'r')
         # read through file line by lie
-        for line in file.readlines():
-            # split the 
-            frame_id, ped_id, x, y = line.split(' ')
-            # convert to floats
-            frame_id, ped_id, x, y = float(frame_id), float(ped_id), float(x), float(y)
-            x,y = y,x # flip the x and y coordinates back to normal
-            # convert the frame_id to time
-            time = frame_id/self.frame_rate + self.cur_time
-            # create an AgentState object
-            pose = ObjectPose(t=time, x=x, y=y, z=0, yaw=0, pitch=0, roll=0, frame=ObjectFrameEnum.START)
+        # for line in file.readlines():
+        #     # split the 
+        #     frame_id, ped_id, x, y = line.split(' ')
+        #     # convert to floats
+        #     frame_id, ped_id, x, y = float(frame_id), float(ped_id), float(x), float(y)
+        #     x,y = y,x # flip the x and y coordinates back to normal
+        #     # convert the frame_id to time
+        #     time = frame_id/self.frame_rate + self.cur_time
+        #     # create an AgentState object
+        #     pose = ObjectPose(t=time, x=x, y=y, z=0, yaw=0, pitch=0, roll=0, frame=ObjectFrameEnum.START)
 
-            print ('pose xy:', x, y)
+        #     print ('pose xy:', x, y)
 
-            # dimensions of a pedestrian(not accurate)
-            l = 1
-            w = 1
-            h = 1.7
-            dims = (w, h, l) 
-            #velocity = estimate velocity from past few frames
-            agent_state = AgentState(pose=pose,dimensions=dims,outline=None,type=AgentEnum.PEDESTRIAN,activity=AgentActivityEnum.MOVING,velocity=(0,0,0),yaw_rate=0)
-            agent_dict[ped_id].append(agent_state)
-            # add the agent state to the list of agent states
+        #     # dimensions of a pedestrian(not accurate)
+        #     l = 1
+        #     w = 1
+        #     h = 1.7
+        #     dims = (w, h, l) 
+        #     #velocity = estimate velocity from past few frames
+        #     agent_state = AgentState(pose=pose,dimensions=dims,outline=None,type=AgentEnum.PEDESTRIAN,activity=AgentActivityEnum.MOVING,velocity=(0,0,0),yaw_rate=0)
+        #     agent_dict[ped_id].append(agent_state)
+        #     # add the agent state to the list of agent states
+
+        agent_list = []
+        # sample_model_3D: 5 x ped_id x 12 x 2
+        for traj in range(sample_model_3D.shape[0]):
+            # Create the dictionary of pedestrian-future AgentState lists for the current trajectory
+            agent_dict = defaultdict(list) # Key: Pedestrian ID | Value: List of AgentStates for each future frame
+            for ped_id in range(sample_model_3D.shape[1]):
+                for future_frame_id in range(sample_model_3D.shape[2]):
+                    # flip the x- and y-coordinates back to normal
+                    x = sample_model_3D[traj][ped_id][future_frame_id][1]
+                    y = sample_model_3D[traj][ped_id][future_frame_id][0]
+
+                    # convert the frame_id to time
+                    frame_id = frame + future_frame_id + 1
+                    time = frame_id / self.frame_rate + self.cur_time
+
+                    # create an AgentState object
+                    pose = ObjectPose(t=time, x=x, y=y, z=0, yaw=0, pitch=0, roll=0, frame=ObjectFrameEnum.START)
+
+                    print("pose xy: ", x, y)
+
+                    # dimensions of a pedestrian (not accurate)
+                    l = 1
+                    w = 1
+                    h = 1.7
+                    dims = (w, h, l)
+                    # velocity = esimate velocity from past few frames
+                    agent_state = AgentState(pose=pose, dimension=dims, outline=None, type=AgentEnum.PEDESTRIAN, activity=AgentActivityEnum.MOVING, velocity=(0, 0, 0), yaw_rate=0)
+                    agent_dict[ped_id].append(agent_state)
+            agent_list.append(agent_dict)
         return agent_dict
             
 
@@ -108,10 +145,11 @@ class PedestrianTrajPrediction(Component):
         # write data to file which model will read
 
         # run the traj prediction model on data
-        run_model(self.model_process)
+        sample_model_3D, frame = run_model(self.model_process)
 
         # read data from model's output file
-        self.convert_data_from_model_output(file)
+        # self.convert_data_from_model_output(file)
+        self.convert_data_from_model_output(sample_model_3D, frame)
 
         # output frame 7/2.5 -> time  + cur_time = detection_time
 
