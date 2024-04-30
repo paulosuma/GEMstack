bad9d413b4e59a8ae7826ce2bf93b27ed034b014
Vishakh Suresh Babu <vishakhsuresh98@gmail.com>
2024-04-29 20:59:56 -0500

    Invoked agent and sign detectors in the main detection program

--- GEMstack/onboard/perception/detection.py
+++ GEMstack/onboard/perception/detection.py
@@ -3,12 +3,13 @@ from ...utils import settings
 from ..interface.gem import GEMInterface
 from ..component import Component
 from .agent_detection import AgentDetector
+from .sign_detection import SignDetector
 
 from ultralytics import YOLO
 import cv2
 import numpy as np
 from typing import Dict
-import threading
+import multiprocessing
 import copy
 import time
 
@@ -16,12 +17,11 @@ import time
 class Detector(Component):
     def __init__(self, vehicle_interface : GEMInterface):
         self.vehicle_interface = vehicle_interface
-        
+
         self.camera_image = None
         self.lidar_point_cloud = None
         
-        self.agent_states = {}
-        self.agent_counter = 0
+        self.object_states = {}
 
     def rate(self):
         return settings.get('perception.rate')
@@ -30,7 +30,7 @@ class Detector(Component):
         return ['vehicle']
     
     def state_outputs(self):
-        return ['agents']
+        return ['states']
     
     def initialize(self):
         # use image_callback whenever 'front_camera' gets a reading, and it expects images of type cv2.Mat
@@ -53,19 +53,35 @@ class Detector(Component):
         # self.save_data()
 
         agent_detector = AgentDetector(vehicle, self.camera_image, self.lidar_point_cloud)
+        sign_detector = SignDetector(vehicle, self.camera_image, self.lidar_point_cloud)
+
+        mp_dict = multiprocessing.Manager().dict() # to get states from the detection modules
+
+        def run_agent_detection():
+            mp_dict['agents'] = agent_detector.detect_agents()
+
+        def run_sign_detection():
+            mp_dict['signs'] = sign_detector.detect_signs()
+
+        p1 = multiprocessing.Process(target=run_agent_detection)
+        p2 = multiprocessing.Process(target=run_sign_detection)
 
         t1 = time.time()
-        detected_agents = agent_detector.detect_agents()
+
+        # start the detection processes
+        p1.start()
+        p2.start()
+
+        # wait for them complete
+        p1.join()
+        p2.join()
 
         t2 = time.time()
-        agent_states, agent_counter = agent_detector.track_agents(detected_agents, self.agent_states, 
-                                                                  self.agent_counter, self.rate())
-        t3 = time.time()
 
-        print('Agent detection time:', t2 - t1)
-        print('Agent shape estimation and tracking time:', t3 - t2)
+        print('Detection time:', t2 - t1)
 
-        self.agent_states = agent_states
-        self.agent_counter = agent_counter
+        self.object_states = mp_dict['agents'] + mp_dict['signs']
 
-        return agent_states
+        # incorporate agent tracking for demo?
+
+        return self.object_states
